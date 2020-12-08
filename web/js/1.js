4c73981a142021b5b72dceab9a2765c98257bd53
luochongfei <luochongfei@globalegrow.com>
2020-12-08 15:41:17 +0800

    i

---
+++ web/js/1.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: www
+ * @Date: 2020-12-07 08:38:45
+ * @LastEditors: luochongfei
+ * @LastEditTime: 2020-12-08 15:40:09
+ */
+
+setTimeout(() => {
+   document.body.innerHTML = window.location.href;
+}, 3000);
